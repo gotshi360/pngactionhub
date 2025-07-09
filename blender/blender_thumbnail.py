aa2eca5b6f2dc02498fdb18dc439a6853b2e7031
Balázs Szöllőskei <balazs.szolloskei@playngo.com>
2025-07-09 23:20:27 +0200

    line change - added info

--- blender/blender_thumbnail.py
+++ blender/blender_thumbnail.py
@@ -22,7 +22,7 @@ def show_dialog():
     dialog.icon = ctx.icon
     dialog.add_text("Due to maintenance, GITEA is currently not functioning and files cannot be accessed")
     dialog.add_text("from within Anchorpoint. Please do not sync in Anchorpoint until the maintenance is over.")  
-    dialog.add_text("You can still work on your files which you have on your local drive, just don't push it to the server. ")
+    dialog.add_info("You can still work on your files which you have on your local drive, just don't push it to the server. ")
     dialog.add_text(" ")
     dialog.add_text("Thank you for your patience.")
     dialog.add_info("Play'n GO Anchorpoint Support")
