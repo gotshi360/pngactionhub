52fcca90caeb4117efbb9fe6d7c29d3bc92dfd6c
Balázs Szöllőskei <balazs.szolloskei@playngo.com>
2025-07-10 00:12:24 +0200

    message changes

--- blender/blender_thumbnail.py
+++ blender/blender_thumbnail.py
@@ -4,25 +4,14 @@ ctx = ap.get_context()
 ui = ap.UI()
 path = ctx.path
 
-
-def create_file(dialog):
-    file_name = dialog.get_value("file_name")
-    content = dialog.get_value("content")
-    with open(os.path.join(path, file_name), "w") as f:
-        f.write(content)
-
-    dialog.close()
-    ap.UI().show_success(f"File {file_name} created")
-
-
 # Defines and shows the pages dialog
 def show_dialog():
     dialog = ap.Dialog()
-    dialog.title = "MAINTENANCE IN PROGRESS!"
+    dialog.title = "Maintenance Notice"
     dialog.icon = ctx.icon
-    dialog.add_text("Due to maintenance, GITEA is currently not functioning and files cannot be accessed")
-    dialog.add_text("from within Anchorpoint. Please do not sync in Anchorpoint until the maintenance is over.")  
-    dialog.add_info("You can still work on your files which you have on your local drive, just don't push it to the server. ")
+    dialog.add_text("GITEA is currently unavailable due to ongoing maintenance, which temporarily disables file access via Anchorpoint.")
+    dialog.add_text("Please refrain from syncing in Anchorpoint until the maintenance is complete.")
+    dialog.add_info("You may continue working on your local files, but avoid pushing any changes to the server during this period.")
     dialog.add_text(" ")
     dialog.add_text("Thank you for your patience.")
     dialog.add_info("Play'n GO Anchorpoint Support")
