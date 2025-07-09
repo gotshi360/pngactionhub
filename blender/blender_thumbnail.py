6a52c4155ced5538560ef0fd1214a139f8e1487c
Balázs Szöllőskei <balazs.szolloskei@playngo.com>
2025-07-09 23:18:47 +0200

    line change.

--- blender/blender_thumbnail.py
+++ blender/blender_thumbnail.py
@@ -20,8 +20,8 @@ def show_dialog():
     dialog = ap.Dialog()
     dialog.title = "MAINTENANCE IN PROGRESS!"
     dialog.icon = ctx.icon
-    dialog.add_text("Due to maintenance, GITEA is currently not functioning and files cannot be accessed from within Anchorpoint.")
-    dialog.add_text("Please do not sync in Anchorpoint until the maintenance is over.")  
+    dialog.add_text("Due to maintenance, GITEA is currently not functioning and files cannot be accessed")
+    dialog.add_text("from within Anchorpoint. Please do not sync in Anchorpoint until the maintenance is over.")  
     dialog.add_text("You can still work on your files which you have on your local drive, just don't push it to the server. ")
     dialog.add_text(" ")
     dialog.add_text("Thank you for your patience.")
