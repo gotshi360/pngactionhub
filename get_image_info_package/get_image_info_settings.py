dc7b2ecac01533194ae67afd321f3cc1b4f2efa4
Balázs Szöllőskei <balazs.szolloskei@playngo.com>
2025-09-14 00:34:14 +0200

    get image info: settings fixes

--- get_image_info_package/get_image_info_settings.py
+++ get_image_info_package/get_image_info_settings.py
@@ -16,9 +16,9 @@ def store_settings(dialog: ap.Dialog):
 dialog = ap.Dialog()
 dialog.title = "Get Image Info Settings"
 
-dialog.add_checkbox("Show Dimensions", settings.get("show_dimensions", True), var="show_dimensions")
-dialog.add_checkbox("Show Resolution", settings.get("show_resolution", True), var="show_resolution")
-dialog.add_checkbox("Show Bit Depth", settings.get("show_bit_depth", True), var="show_bit_depth")
+dialog.add_text("Show Dimensions").add_checkbox(default=settings.get("show_dimensions", True), var="show_dimensions")
+dialog.add_text("Show Resolution").add_checkbox(default=settings.get("show_resolution", True), var="show_resolution")
+dialog.add_text("Show Bit Depth").add_checkbox(default=settings.get("show_bit_depth", True), var="show_bit_depth")
 
 dialog.add_button("Save", callback=store_settings)
 
